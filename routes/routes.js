508c181f737931f807d577ebfc97edb409c9179d
MagicDev119 <melnykovychadrian@gmail.com>
2022-11-17 11:18:44 +0300

    update scraping

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const router = express.Router()
-const dataPath = '../pages/pages.json';
+const dataPath = './pages/pages.json';
 router.get('/getVehicleList', (req, res) => {
   fs.readFile(dataPath, 'utf8', (err, data) => {
     if (err) {
